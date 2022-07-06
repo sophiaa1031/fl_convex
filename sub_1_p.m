b50247c61b9cf40fc24321a47735bbee83c08cba
sophiaa1031 <798167982@qq.com>
2022-07-06 10:38:57 +0800

    fix the bug for not convergence

--- sub_1_p.m
+++ sub_1_p.m
@@ -1,4 +1,4 @@
-clc;clear;close
+%% clc;clear;close
 
 m=50; %Users' total number
 iterationNumber=10;    %Maximun iteration times
@@ -13,7 +13,7 @@ q = rand(1,m);%RB assignment
 Dia = 15; %diameter of the BS
 z = 28.1; %model size
 T_max = 35*ones(1,m); %maximum latency in one round
-erequirement=0.3*ones(1,m); %% energy requirement
+erequirement=0.7*ones(1,m); %% energy requirement
 kapa = 1e-28;%processor coefficient
 D = 128;%sample size
 c = 1.68e7;%cpu bits
@@ -22,17 +22,22 @@ f=normrnd(10^9,10^8,1,m);  %CPU frequency
 distance = normrnd(7,2,1,m); %distance to the BS
 v = normrnd(13,5,1,m); %velocity
 
+% f= 7e8+(1e9-7e8)*rand(1,m);
+% distance = 1+12.*rand(1,m);
+% v = 30.*rand(1,m);
 
-p = ones(1,m)*0.1;
-lambda = ones(m,1)*0.05;   %拉格朗日乘子初始化 
-real_ite=1;
-G_v = [];  lambda_v = [];  L_v = [];  obj_v = [];
-tol = 10^(-4);
+p = ones(1,m)*0.1; % 决策变量功率p初始化
+lambda = ones(m,1)*0.05;   % 拉格朗日乘子lambda初始化 
+real_ite=1; % 当前迭代次数
+G_v = [];  
+lambda_v = [];  
+L_v = [];  % 拉格朗日函数
+obj_v = []; 
+tol = 10^(-4); % 判断收敛的阈值
 t_lambda=0.01;  % 拉格朗日乘子的更新步长
-t_x=0.1;      % 决策变量 x 的更新步长
-max_iteration = 3e4;
+t_x=0.01;      % 决策变量 x 的更新步长
+max_iteration = 3e4;  % 最大迭代次数
 
-theta = 1;
 for i = 1 : max_iteration
     %% 迭代更新功率P
     for k=1:m
@@ -42,31 +47,31 @@ for i = 1 : max_iteration
         H_3 = hk/(I+bkn0);
         H_4 = -H_2*H_3*p(k)/log(2)/(1+H_3*p(k))/(log(1+H_3*p(k))/log(2))^2+...
             H_2/(log(1+H_3*p(k))/log(2));
-        part_p = -H_1*theta*exp(H_1/p(k))+lambda(k)*H_4;
+        part_p = -H_1*exp(H_1/p(k))/(q(k)*(p(k))^2)+lambda(k)*H_4;
         
         p(k) = p(k)-t_x*part_p;
     end
     %% 迭代更新拉格朗日乘子 lambda
-for k=1:m
-    Grad_f(k) = p(k)*z*inv_pos(q(k)*b*log(1+p(k)*hk/(I+bkn0))/log(2))+itr*kapa*D*c*f(k)^2 - erequirement(k);
-end
-lambda = max( lambda + t_lambda * Grad_f' , zeros( m,1) );
+    for k=1:m
+        Grad_f(k) = p(k)*z*inv_pos(q(k)*b*log(1+p(k)*hk/(I+bkn0))/log(2))+itr*kapa*D*c*f(k)^2 - erequirement(k);
+    end
+    lambda = max( lambda + t_lambda * Grad_f' , zeros( m,1) );
 
-for k=1:m
-Term1(k) = 2^(z/(b*q(k)*(T_max(k)-itr*D*c/f(k)))-1);
-end
-obj = 0;
-for k=1:m
-    obj = obj + exp((I+bkn0)*Term1(k)*inv_pos(p(k))/(distance(k)^(-gamma)))/q(k);
-end
-
-obj = obj*theta;
-L = obj+sum(Grad_f * lambda);
-    %% 记录历史更新结果
+    for k=1:m
+    Term1(k) = 2^(z/(b*q(k)*(T_max(k)-itr*D*c/f(k)))-1);
+    end
+    obj = 0;
+    for k=1:m
+        obj = obj + exp((I+bkn0)*Term1(k)*inv_pos(p(k))/(distance(k)^(-gamma)))/q(k);
+    end
+    
+    % 拉格朗日函数
+    L = obj+sum(Grad_f * lambda);
+    % 记录历史更新结果
     G_v = [G_v Grad_f];%乘子的梯度
     lambda_v = [lambda_v lambda];%乘子的值
-    obj_v = [obj_v obj]; 
-    L_v = [L_v L];
+    obj_v = [obj_v obj]; % obj_v append
+    L_v = [L_v L]; % L_v append
     %% 终止条件判断
     if (real_ite>2) && ( abs( obj - L ) < tol )  % k == 5000 %k>3&&(f_v(k)-f_v(k-1)<tol)
         break;
@@ -82,8 +87,15 @@ disp(['迭代次数为：',num2str(real_ite),'次']);
 disp(['最优值点为：']);
 p
 
+%% 
 figure(1)
-plot( 2:length(L_v),L_v(2:length(L_v)), '-ro', 'linewidth',2)
+plot( 5400:length(L_v),L_v(5400:length(L_v)), '-ro', 'linewidth',2)
+grid on
+xlabel('Iteration round');
+ylabel('Lagrange function');
 
 figure(2)
 plot( 2:length(L_v), obj_v(2:length(obj_v)) - L_v(2:length(L_v)) , '-b*', 'linewidth',2)
+grid on
+xlabel('Iteration round');
+ylabel('Objective - Lagrange function');
